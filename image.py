3513107d945ebe5d733f218ee112966a7ba1b601
dastarruer <ayushpramanik399@gmail.com>
2024-11-16 17:10:16 -0400

    Added the Pexels API to get random stock photos, added an API key

--- image.py
+++ image.py
@@ -1,1 +1,8 @@
-from PIL import Image 
+from requests import get
+from api import API_KEY
+
+headers = {
+    "Authorization": API_KEY
+}
+
+print(get("https://api.pexels.com/v1/curated?page=2&per_page=1", headers=headers).json())
