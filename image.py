8c699ed259da8e3ff599e598e4478e1ad4105771
dastarruer <ayushpramanik399@gmail.com>
2024-11-16 17:35:13 -0400

    Added ability to download images using the Pexels API

--- image.py
+++ image.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from requests import get
 from api import API_KEY
 
@@ -5,4 +6,9 @@ headers = {
     "Authorization": API_KEY
 }
 
-print(get("https://api.pexels.com/v1/curated?page=2&per_page=1", headers=headers).json())
+photo = get("https://api.pexels.com/v1/curated?page=1&per_page=1", headers=headers).json() 
+photo_url = photo["photos"][0]["src"]["original"]
+photo = get(photo_url)
+
+with open("image.JPEG", 'wb') as f:
+    f.write(photo.content)
