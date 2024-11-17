2ed14871a94427a50c6b0c2b2499993e6ede5165
dastarruer <ayushpramanik399@gmail.com>
2024-11-16 22:03:47 -0400

    Added more categories that can be searched for. Program will choose a random one, and search for it, which provides more variety in the photos chosen. However, feel like there could be more categories, which I may or may not add later on.

--- image.py
+++ image.py
@@ -1,11 +1,24 @@
-from requests import get
 from api import API_KEY
 
+from random import choice
+from requests import get
+
+# This header is used to send our API key to the API for verification
 headers = {
     "Authorization": API_KEY
 }
 
-api_query = "https://api.pexels.com/v1/search?query=abstract&orientation=landscape&per_page=1"
+# The parameters that will be sent to the API
+categories = ["abstract", "landscape", "ocean", "sky"]
+num_of_photos = "1"
+orientation = "landscape"
+
+# The category that the API will search for
+chosen_category = choice(categories)
+
+print(f"Searching for '{chosen_category}'...")
+
+api_query = f"https://api.pexels.com/v1/search?query={chosen_category}&orientation={orientation}&per_page={num_of_photos}"
 
 photo = get(api_query, headers=headers).json() 
 
@@ -14,3 +27,5 @@ photo = get(photo_url)
 
 with open("image.JPEG", 'wb') as f:
     f.write(photo.content)
+
+print("Done!")
