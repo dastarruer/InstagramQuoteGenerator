b381d8991de1d95dd12eac814da86a9583889685
dastarruer <ayushpramanik399@gmail.com>
2024-11-18 09:18:32 -0400

    Added functionality to draw quotee, added getbbox() to get both the height and width

--- edit_photos.py
+++ edit_photos.py
@@ -33,37 +33,51 @@ class PhotoEditor:
         )
         
     
-    def draw_quote(self, quote,):
+    def draw_quote(self, quote, quotee):
         """
         Draw the quote on top of the photo. It is centered in relation to the transparent rectangle that can be drawn with 'draw_rectangle()'.
         """
         font_size = 300
         font = ImageFont.truetype("fonts/georgiaz.ttf", font_size)
-        text_width = font.getlength(quote)
-        
+        quote_bbox = font.getbbox(quote)
+        quote_width = quote_bbox[0] - quote_bbox[2]
+        quote_height = quote_bbox[1] - quote_bbox[3]
+
         rectangle_width = self.rectangle_x1 - self.rectangle_x0
         rectangle_height = self.rectangle_y1 - self.rectangle_y0
               
         # The coordinates of the quote (which is placed in the center)  
-        quote_x = (rectangle_width - text_width) // 2
+        quote_x = (rectangle_width - quote_width) // 2
         quote_y = (rectangle_height) // 2
         quote_xy = (quote_x, quote_y)
+        
+        quotee_x = quote_x
+        quotee_y = quote_y + 10
+        quotee_xy = (0, 0)
 
         quote_fill = (0, 0, 0)
+        quotee_fill = (0, 0, 0)
         self.draw.text(
             quote_xy,
             quote,
             fill=quote_fill,
             font=font
         )
+        
+        self.draw.text(
+            quotee_xy,
+            quotee,
+            fill=quotee_fill,
+            font=font
+        )
     
     
-    def edit_photo(self, quote):
+    def edit_photo(self, quote, quotee):
         """
         Execute both 'draw_rectangle()' and 'draw_quote()'.
         """
         self.draw_rectangle()
-        self.draw_quote(quote)
+        self.draw_quote(quote, quotee)
     
     
     def show_photo(self):
@@ -77,5 +91,5 @@ class PhotoEditor:
 photo = Image.open(filename)
 
 photo_editor = PhotoEditor(photo)
-photo_editor.edit_photo("Neque porro quisquam est...")
+photo_editor.edit_photo("Neque porro quisquam est...", "Me")
 photo_editor.show_photo()
