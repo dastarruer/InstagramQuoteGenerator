aad8dd8b0ef5e21773eac5223227f5cdfe32d795
dastarruer <ayushpramanik399@gmail.com>
2024-11-18 08:45:31 -0400

    Fixed spelling error

--- edit_photos.py
+++ edit_photos.py
@@ -15,7 +15,7 @@ class PhotoEditor:
         
         # This is the rectangle 's xy coordinates
         self.rectangle_x0 = rectangle_x_offset
-        self.rectanlge_x1 = self.width - rectangle_x_offset
+        self.rectangle_x1 = self.width - rectangle_x_offset
         self.rectangle_y0 = rectangle_y_offset
         self.rectangle_y1 = self.height - rectangle_y_offset
         
@@ -24,7 +24,7 @@ class PhotoEditor:
         """
         Draw a semi-transparent rectangle over the photo, similar to how quotes are displayed on Instagram. This effect mimics the style where stock photos are paired with a transparent rectangle, and text is written on top of it for better readability.
         """
-        xy = (self.rectangle_x0, self.rectangle_y0, self.rectanlge_x1, self.rectangle_y1)
+        xy = (self.rectangle_x0, self.rectangle_y0, self.rectangle_x1, self.rectangle_y1)
         transparency = 200
         self.draw.rectangle(
             xy = xy, 
@@ -40,8 +40,8 @@ class PhotoEditor:
         font_size = 300
         font = ImageFont.truetype("fonts/georgiaz.ttf", font_size)
         text_width = font.getlength(quote)
-
-        x_center = (self.rectanlge_x1 - self.rectangle_x0 - text_width) // 2
+        
+        x_center = (self.rectangle_x1 - self.rectangle_x0 - text_width) // 2
         y_center = (self.rectangle_y1 - self.rectangle_y0) // 2
         position_center = (x_center, y_center)
 
