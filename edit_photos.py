f935ceeb587a2f72e91979d7501aed8ac6c8e6ab
dastarruer <ayushpramanik399@gmail.com>
2024-11-18 09:28:07 -0400

    Added proper positioning for quotee

--- edit_photos.py
+++ edit_photos.py
@@ -37,23 +37,30 @@ class PhotoEditor:
         """
         Draw the quote on top of the photo. It is centered in relation to the transparent rectangle that can be drawn with 'draw_rectangle()'.
         """
-        font_size = 300
-        font = ImageFont.truetype("fonts/georgiaz.ttf", font_size)
-        quote_bbox = font.getbbox(quote)
-        quote_width = abs(quote_bbox[0] - quote_bbox[2])
-        quote_height = abs(quote_bbox[1] - quote_bbox[3])
-
         rectangle_width = self.rectangle_x1 - self.rectangle_x0
         rectangle_height = self.rectangle_y1 - self.rectangle_y0
+        
+        quote_font_size = 300
+        quote_font = ImageFont.truetype("fonts/georgiaz.ttf", quote_font_size)
+        
+        quote_bbox = quote_font.getbbox(quote)
+        quote_width = abs(quote_bbox[0] - quote_bbox[2])
+        quote_height = abs(quote_bbox[1] - quote_bbox[3])
               
-        # The coordinates of the quote (which is placed in the center)  
         quote_x = (rectangle_width - quote_width) // 2
-        quote_y = (rectangle_height) // 2
+        quote_y = (rectangle_height - quote_height) // 2
         quote_xy = (quote_x, quote_y)
         
-        quotee_x = quote_x
-        quotee_y = quote_y + 10
-        quotee_xy = (0, 0)
+        quotee_font_size = 250
+        quotee_font = ImageFont.truetype("fonts/georgiaz.ttf", quotee_font_size)
+        
+        quotee_bbox = quotee_font.getbbox(quotee)
+        quotee_width = abs(quotee_bbox[0] - quotee_bbox[2])
+        quotee_height = abs(quotee_bbox[1] - quotee_bbox[3])
+
+        quotee_x = (rectangle_width - quotee_width) // 2
+        quotee_y = (quote_y + quote_height / 2) + quotee_height
+        quotee_xy = (quotee_x, quotee_y)
 
         quote_fill = (0, 0, 0)
         quotee_fill = (0, 0, 0)
@@ -61,14 +68,14 @@ class PhotoEditor:
             quote_xy,
             quote,
             fill=quote_fill,
-            font=font
+            font=quote_font
         )
         
         self.draw.text(
             quotee_xy,
             quotee,
             fill=quotee_fill,
-            font=font
+            font=quote_font
         )
     
     
