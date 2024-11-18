405fc27dd4809c4d6f4c22a88bb9072dfe040b96
dastarruer <ayushpramanik399@gmail.com>
2024-11-18 08:44:35 -0400

    Added docstrings and comments

--- edit_photos.py
+++ edit_photos.py
@@ -9,10 +9,11 @@ class PhotoEditor:
         self.width, self.height = photo.size
         self.draw = ImageDraw.Draw(photo, "RGBA")
         
-        
+        # This is the offset of the rectangle that is drawn over the photo for better readability
         rectangle_x_offset = 50
         rectangle_y_offset = 100 
         
+        # This is the rectangle 's xy coordinates
         self.rectangle_x0 = rectangle_x_offset
         self.rectanlge_x1 = self.width - rectangle_x_offset
         self.rectangle_y0 = rectangle_y_offset
@@ -25,7 +26,6 @@ class PhotoEditor:
         """
         xy = (self.rectangle_x0, self.rectangle_y0, self.rectanlge_x1, self.rectangle_y1)
         transparency = 200
-
         self.draw.rectangle(
             xy = xy, 
             fill = (255, 255, 255, transparency), 
@@ -34,6 +34,9 @@ class PhotoEditor:
         
     
     def draw_quote(self, quote):
+        """
+        Draw the quote on top of the photo. It is centered on top of the transparent rectangle that can be drawn with 'draw_rectangle()'.
+        """
         font_size = 300
         font = ImageFont.truetype("fonts/georgiaz.ttf", font_size)
         text_width = font.getlength(quote)
@@ -51,11 +54,17 @@ class PhotoEditor:
     
     
     def edit_photo(self, quote):
+        """
+        Execute both 'draw_rectangle()' and 'draw_quote()'.
+        """
         self.draw_rectangle()
         self.draw_quote()
     
     
     def show_photo(self):
+        """
+        Show the photo.
+        """
         self.photo.show()
 
 
