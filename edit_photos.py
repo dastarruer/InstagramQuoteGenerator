68bb3c4569f27049e718a47abf3631efa7c9949e
dastarruer <ayushpramanik399@gmail.com>
2024-11-18 13:30:55 -0400

    Used Text class to get the width/height of the quote and quotee text

--- edit_photos.py
+++ edit_photos.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageFont, ImageDraw
 filename = "image.JPEG"
 
 class Text:
-    def __init__(self, text, font):
+    def __init__(self, text: str, font: ImageFont):
         self.text = text
         self.font = font
         
@@ -14,7 +14,7 @@ class Text:
 
 
 class PhotoEditor:
-    def __init__(self, photo):
+    def __init__(self, photo: Image):
         self.photo = photo
         self.width, self.height = photo.size
         self.draw = ImageDraw.Draw(photo, "RGBA")
@@ -52,24 +52,18 @@ class PhotoEditor:
         
         quote_font_size = 300
         quote_font = ImageFont.truetype("fonts/georgia.ttf", quote_font_size)
-        
-        quote_bbox = quote_font.getbbox(quote)
-        quote_width = abs(quote_bbox[0] - quote_bbox[2])
-        quote_height = abs(quote_bbox[1] - quote_bbox[3])
+        quote_text = Text(quote, quote_font)
               
-        quote_x = (rectangle_width - quote_width) // 2
-        quote_y = (rectangle_height - quote_height) // 2
+        quote_x = (rectangle_width - quote_text.width) // 2
+        quote_y = (rectangle_height - quote_text.height) // 2
         quote_xy = (quote_x, quote_y)
         
         quotee_font_size = 250
         quotee_font = ImageFont.truetype("fonts/georgiai.ttf", quotee_font_size)
-        
-        quotee_bbox = quotee_font.getbbox(quotee)
-        quotee_width = abs(quotee_bbox[0] - quotee_bbox[2])
-        quotee_height = abs(quotee_bbox[1] - quotee_bbox[3])
+        quotee_text = Text(quotee, quotee_font)
 
-        quotee_x = (rectangle_width - quotee_width) // 2
-        quotee_y = (quote_y + quote_height / 2) + quotee_height
+        quotee_x = (rectangle_width - quotee_text.width) // 2
+        quotee_y = (quote_y + quote_text.height / 2) + quotee_text.height
         quotee_xy = (quotee_x, quotee_y)
 
         quote_fill = (0, 0, 0)
