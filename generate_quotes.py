926cdef2862bdac28698f54371b618c212e61c9f
dastarruer <ayushpramanik399@gmail.com>
2024-11-22 15:02:32 -0400

    Switched to API Ninja for quotes

--- generate_quotes.py
+++ generate_quotes.py
@@ -1,3 +1,16 @@
-from requests import get
+from api import API_NINJAS_KEY
 
-print(get("https://api.quotable.io/quotes/random").json())
+from requests import get, codes
+
+
+category = 'happiness'
+api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
+headers = {
+    'X-Api-Key':API_NINJAS_KEY
+}
+
+r = get(api_url, headers=headers)
+if r.status_code == codes.ok:
+    print(r.json())
+else:
+    print(r.status_code)
