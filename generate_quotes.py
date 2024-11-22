89f08cd8bfa0c4187b289ce61b018ad7604aa441
dastarruer <ayushpramanik399@gmail.com>
2024-11-22 15:35:25 -0400

    Renamed 'r' to 'quote', changed 'quote = quote.json()' to 'quote = quote.json()[0]'

--- generate_quotes.py
+++ generate_quotes.py
@@ -13,11 +13,11 @@ class QuoteGenerator:
         }
         api_url = f'https://api.api-ninjas.com/v1/quotes?category={category}'
 
-        r = get(api_url, headers=headers)
-        if r.status_code == codes.ok:
-            r = r.json()
-            return (r[0]["quote"], r[0]["author"])
-        print(f"Something went wrong... ({r.status_code})")
+        quote = get(api_url, headers=headers)
+        if quote.status_code == codes.ok:
+            quote = quote.json()[0]
+            return (quote["quote"], quote["author"])
+        print(f"Something went wrong... ({quote.status_code})")
         
         
 q = QuoteGenerator()
