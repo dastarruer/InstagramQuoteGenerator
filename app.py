61aa42f8702b01c67e6b15a09773cecc6517fe23
dastarruer <ayushpramanik399@gmail.com>
2024-11-22 22:33:13 -0400

    Extracted index() function to generate_photo()

--- app.py
+++ app.py
@@ -8,12 +8,22 @@ from PIL import Image
 app = Flask(__name__)
 
 
+def generate_photo(filename):
+    """
+    Generate a photo by overlaying a quote onto an image sourced from Pexels, then save the edited image to 'filename'.
+    """
+    random_quote = quote_generator.generate_quote("anger")
+    quote = random_quote[0]
+    author = random_quote[1]
+    photo_generator.get_pexels_image("1")
+    
+    img = Image.open(filename)
+    photo_editor = PhotoEditor(img)
+    photo_editor.edit_photo(quote, author, filename)
+
+
 @app.route('/')
 def index():
     filename = "static/image.JPEG"
-    quote = quote_generator.generate_quote("anger")
-    photo = photo_generator.get_pexels_image("1")
-    img = Image.open(filename)
-    photo_editor = PhotoEditor(img)
-    photo_editor.edit_photo(quote[0], quote[1], filename)
+    create_photo(filename)    
     return render_template("index.html")
