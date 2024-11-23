cf6c39f37885ccca9f7ce5dc04c5d8a9f74f73a1
dastarruer <ayushpramanik399@gmail.com>
2024-11-22 22:18:02 -0400

    Regenerate a new quote every time the page is reloaded

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from edit_photos import photo_editor
+from edit_photos import PhotoEditor
 from generate_photos import photo_generator
 from generate_quotes import quote_generator
 
 from flask import Flask, render_template
+from PIL import Image
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    filename = "static/image.JPEG"
+    quote = quote_generator.generate_quote("anger")
+    photo = photo_generator.get_pexels_image("1")
+    img = Image.open(filename)
+    photo_editor = PhotoEditor(img)
+    photo_editor.edit_photo(quote[0], quote[1], filename)
     return render_template("index.html")
