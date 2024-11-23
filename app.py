aa257f8fbbd3b39a28593266147665bc6dcda0c2
dastarruer <ayushpramanik399@gmail.com>
2024-11-22 22:44:31 -0400

    Added comments

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ from generate_quotes import quote_generator
 from flask import Flask, render_template
 from PIL import Image
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 def generate_photo(filename):
     """
     Generate a photo by overlaying a quote onto an image sourced from Pexels, then save the edited image to 'filename'.
     """
+    # Get the quote
     random_quote = quote_generator.generate_quote("anger")
     quote = random_quote[0]
     author = random_quote[1]
+    
+    # Save a random iamge
     photo_generator.get_pexels_image("1")
     
+    # Edit the photo
     photo_editor = PhotoEditor(filename)
     photo_editor.edit_photo(quote, author, filename)
 
