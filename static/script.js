234cfdf62aad08cd69a046fa6d2d59fb082f7708
dastarruer <ayushpramanik399@gmail.com>
2024-11-26 17:57:39 -0400

    User can now toggle between 'create-quote' and 'random-quote'

--- static/script.js
+++ static/script.js
@@ -1,13 +1,35 @@
-const customQuoteButton = document.getElementById("custom-quote-show");
+const showCustomQuote = document.getElementById("custom-quote-show");
+const showRandomQuote = document.getElementById("random-quote-show");
 
-customQuoteButton.addEventListener("click", (event) => {
+// When the 'custom-quote-show' button is pressed, hide the random quote and show the custom quote section
+showCustomQuote.addEventListener("click", (event) => {
     hideRandomQuoteDiv();
-    const customQuote = document.getElementById("custom-quote");
-    customQuote.hidden = false;
+    showCustomQuoteDiv();
 });
+
+showRandomQuote.addEventListener("click", (event) => {
+    showRandomQuoteDiv();
+    hideCustomQuoteDiv();
+});
+
+function hideCustomQuoteDiv() {
+    const customQuote = document.getElementById("custom-quote");
+    customQuote.hidden = true;
+}
 
 function hideRandomQuoteDiv() {
     const newQuote = document.getElementById("new-quote");
     newQuote.hidden = true;
-    customQuoteButton.hidden = true;
+    showCustomQuote.hidden = true;
+}
+
+function showCustomQuoteDiv() {
+    const customQuote = document.getElementById("custom-quote");
+    customQuote.hidden = false;
+}
+
+function showRandomQuoteDiv() {
+    const newQuote = document.getElementById("new-quote");
+    newQuote.hidden = false;
+    showCustomQuote.hidden = false;
 }
