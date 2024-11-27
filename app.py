897111c192345d96547671347c84607fe78678a9
dastarruer <ayushpramanik399@gmail.com>
2024-11-26 22:19:29 -0400

    Added ability to create custom quotes

--- app.py
+++ app.py
@@ -11,19 +11,28 @@ SAVE_AS = "static/images/edited_image.JPEG"
 app = Flask(__name__)
 
 
+def generate_custom_photo_quote(quote, author, filename, save_as):
+    # Save a random iamge from the Pexels API
+    photo_generator.save_pexels_image(1)
+    
+    # Edit the photo and save it
+    photo_editor = PhotoEditor(filename, save_as)
+    photo_editor.edit_photo(quote, author)
+
+
 def generate_random_photo_quote(filename, save_as):
     """
-    Generate a photo by overlaying a quote onto an image sourced from Pexels, then save the edited image to 'filename'.
+    Generate a photo by overlaying a random quote from the API Ninjas Quote API onto an image sourced from the Pexels API, then save the edited image to 'filename'.
     """
     # Get the quote
     random_quote = quote_generator.generate_quote("anger")
     quote = random_quote[0]
     author = random_quote[1]
     
-    # Save a random iamge
+    # Save a random iamge from the Pexels API
     photo_generator.save_pexels_image(1)
     
-    # Edit the photo
+    # Edit the photo and save it
     photo_editor = PhotoEditor(filename, save_as)
     photo_editor.edit_photo(quote, author)
 
@@ -33,7 +42,7 @@ def index():
     if request.method == 'POST':
         quote = request.form['quote']
         author = request.form['author']
+        generate_custom_photo_quote(quote, author, FILENAME, SAVE_AS)
         return render_template("index.html")
-        
     generate_random_photo_quote(FILENAME, SAVE_AS)    
     return render_template("index.html")
