112943ac8fdc5e9ba8a98e2fa59c237dbe75e9b1
dastarruer <ayushpramanik399@gmail.com>
2024-11-27 12:57:34 -0400

    Globals.py

---
+++ globals.py
@@ -0,0 +1,5 @@
+FILENAME = "static/images/image.JPEG"
+SAVE_AS = "static/images/edited_image.JPEG"
+
+PEXELS_KEY = "8lEbyC1QoBv19dh84Jw1SXfhfEzLLmQOKUCC0ghAThtUx4T4jfAib2fj"
+API_NINJAS_KEY = "dipSlkx6rP6FBl+Se/aT8w==SVdz1winVUMAX6h6"
